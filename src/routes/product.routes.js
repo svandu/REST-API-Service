5412ef25ba3c5d27e45c984d8a244e4933ff3991
Surbhi Vandana <svandu08@gmail.com>
2024-01-03 12:56:54 +0530

    delete operation for products done

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,10 @@
 const Router = require("express")
-const {getAllProducts, createNewProduct} = require("../controllers/product.controllers.js")
+const {getAllProducts, createNewProduct, deleteProducts} = require("../controllers/product.controllers.js")
 
 const router = Router()
 
 router.route("/products").get(getAllProducts).post(createNewProduct)
-router.get("/product",(req, res) => {} )
-
+router.route("/products/:productId").delete(deleteProducts)
+// router.get("/product",(req, res) => {} )
+    
 module.exports = router
